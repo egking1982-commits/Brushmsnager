0d21a5cbc45961e25017a28e517a78a4e51e066c
egking1982-commits <egking.1982@gmail.com>
2026-01-01 06:33:45 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'brush-manager-v1';
+const assets = [
+  './',
+  './index.html', // تأكد أن هذا هو اسم ملف الـ HTML الخاص بك
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdn.jsdelivr.net/npm/xlsx/dist/xlsx.full.min.js',
+  'https://www.gstatic.com/firebasejs/9.17.1/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/9.17.1/firebase-database-compat.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
